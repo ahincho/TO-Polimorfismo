66ce9309b5d9074a5db3ac15ba80fd5424421c5e
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2022-11-16 01:52:24 -0500

    Creando Interface Vehiculo con metodos generales

---
+++ Tipo/Java/Vehiculo.java
@@ -0,0 +1,23 @@
+package tipos;
+
+public interface Vehiculo {
+	
+	// Interface Vehiculo y Metodos que se deben implementar
+	
+	public void setMatricula(String m);
+	
+	public String getMatricula();
+	
+	public void setMarca(String m);
+	
+	public String getMarca();
+	
+	public void setModelo(String m);
+	
+	public String getModelo();
+	
+	public void mostrarDatos();
+	
+	public void tocarClaxon();
+	
+}
